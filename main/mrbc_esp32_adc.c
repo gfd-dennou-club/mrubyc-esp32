b7e83fd1a6df2a51087dc5aecf188100d78626e8
rikutonsicloud@gmail.com <rikutonsicloud@gmail.com>
2020-07-16 16:28:01 +0900

    Enable adc2 init and read in ADC.rb

--- main/mrbc_esp32_adc.c
+++ main/mrbc_esp32_adc.c
@@ -17,7 +17,7 @@ static int unreferenced;
 
 static esp_adc_cal_characteristics_t *adc_chars;
 static const adc_unit_t unit1 = ADC_UNIT_1;
-//static const adc_unit_t unit2 = ADC_UNIT_2;
+static const adc_unit_t unit2 = ADC_UNIT_2;
 
 
 /*! メソッド nop(count) 本体 : nop (no operation)
@@ -77,26 +77,31 @@ static void mrbc_esp32_adc_read_adc1(mrb_vm *vm, mrb_value *v, int argc){
 /*
   ADC2 の初期化・計測は未実装
 */
-/*
-static void mrbc_esp32_adc_adc2_init(mrb_vm *vm, mrb_value *v, int argc){
-  adc2_config_channel_atten((adc2_channel_t)channel, atten);
+static void mrbc_esp32_adc_init_adc2(mrb_vm *vm, mrb_value *v, int argc){
+  adc2_channel_t channel = GET_INT_ARG(1);
+  adc_atten_t    atten   = GET_INT_ARG(2);
+  int            width   = GET_INT_ARG(3);
+
+  adc2_config_channel_atten(channel, atten);
   adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
-  esp_adc_cal_characterize(unit2, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+  esp_adc_cal_characterize(unit2, atten, width, DEFAULT_VREF, adc_chars);
 }
-*/
-/*
+
 static void mrbc_esp32_adc_read_adc2(mrb_vm *vm, mrb_value *v, int argc){
   uint32_t adc_reading = 0;
+
+  adc2_channel_t channel = GET_INT_ARG(1);
+  int            width   = GET_INT_ARG(2);
+
   for (int i = 0; i < NO_OF_SAMPLES; i++) {
     int raw;
-    adc2_get_raw((adc2_channel_t)channel, ADC_WIDTH_BIT_12, &raw);
+    adc2_get_raw(channel, width, &raw);
     adc_reading += raw;
   }
   adc_reading /= NO_OF_SAMPLES;
   uint32_t millivolts = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
   SET_INT_RETURN(millivolts);
 }
-*/
 
 /*! クラス定義処理を記述した関数
   この関数を呼ぶことでクラス ADC が定義される
@@ -109,8 +114,8 @@ mrbc_mruby_esp32_adc_gem_init(struct VM* vm)
 /*
 ADC.init_adc1(pin)
 ADC.read_adc1(pin)
-//ADC.init_adc2(pin)
-//ADC.read_adc2(pin)
+ADC.init_adc2(pin)
+ADC.read_adc2(pin)
 */
   // クラス ADC 定義
   mrbc_class_esp32_adc = mrbc_define_class(vm, "ADC", mrbc_class_object);
@@ -118,7 +123,7 @@ ADC.read_adc1(pin)
   // 各メソッド定義（mruby/c ではインスタンスメソッドをクラスメソッドとしても呼び出し可能）
   mrbc_define_method(vm, mrbc_class_esp32_adc, "init_adc1",      mrbc_esp32_adc_init_adc1);
   mrbc_define_method(vm, mrbc_class_esp32_adc, "read_adc1",      mrbc_esp32_adc_read_adc1);
-//  mrbc_define_method(vm, mrbc_class_esp32_adc, "init_adc2",      mrbc_esp32_adc_init_adc2);
-//  mrbc_define_method(vm, mrbc_class_esp32_adc, "read_adc2",      mrbc_esp32_adc_read_adc2);
-  mrbc_define_method(vm, mrbc_class_esp32_adc, "nop",             mrbc_nop);
+  mrbc_define_method(vm, mrbc_class_esp32_adc, "init_adc2",      mrbc_esp32_adc_init_adc2);
+  mrbc_define_method(vm, mrbc_class_esp32_adc, "read_adc2",      mrbc_esp32_adc_read_adc2);
+  mrbc_define_method(vm, mrbc_class_esp32_adc, "nop",            mrbc_nop);
 }
