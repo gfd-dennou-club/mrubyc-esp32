388560071efe2f4e84be95cb2e9946791cba97d6
rikutonsicloud@gmail.com <rikutonsicloud@gmail.com>
2021-02-15 11:39:12 +0900

    [WIP] I2C write, read を実装

--- mrblib/models/i2c.rb
+++ mrblib/models/i2c.rb
@@ -1,7 +1,23 @@
 class I2C
+  # 定数
+  MASTER = 0
+  SLAVE = 1
   def initialize(port, scl, sda)
     @port = port
     @scl  = scl
     @sda  = sda
+  end
+
+  def write(i2c_adrs_7, *data)
+    p data
+    if(data[0].kind_of?(Array))
+      data = data[0]
+    end
+    p data
+    I2C.__write(i2c_adrs_7, data)
+  end
+  
+  def read(i2c_adrs_7, len)
+    I2C.__read(i2c_adrs_7, len)
   end
 end
