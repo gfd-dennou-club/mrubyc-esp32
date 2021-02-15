30dae90ace1d5d9dba5e5e6d7ed39db8ab9054f3
rikutonsicloud@gmail.com <rikutonsicloud@gmail.com>
2021-02-15 16:29:05 +0900

    Fix i2c.rb panic error and Add argument to read function

--- mrblib/models/i2c.rb
+++ mrblib/models/i2c.rb
@@ -13,11 +13,11 @@ class I2C
     if(data[0].kind_of?(Array))
       data = data[0]
     end
-    p data
-    I2C.__write(i2c_adrs_7, data)
+    self.__write(i2c_adrs_7, data)
   end
   
-  def read(i2c_adrs_7, len)
-    I2C.__read(i2c_adrs_7, len)
+  def read(i2c_adrs_7, len, *params)
+    write(i2c_adrs_7, params) unless params.empty?
+    self.__read(i2c_adrs_7, len)
   end
 end
