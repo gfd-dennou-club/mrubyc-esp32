b4eaa5235d6a77d75cc979e99773a13d5d5e032c
Nishikori Koji <webse2mycee5mbg8yrx3@docomo.ne.jp>
2021-09-21 14:29:39 +0900

    [ADD] tof_init() and env2_init() to master.rb

--- mrblib/loops/master.rb
+++ mrblib/loops/master.rb
@@ -1,3 +1,7 @@
+def tof_init(i2c)
+  p 'tof init'
+end
+
 def tof(i2c, display)
   p "tof now"
   sleep 1
@@ -6,6 +10,10 @@ end
 def menu(i2c, display)
   p "menu now"
   sleep 1
+end
+
+def env2_init(i2c)
+  p 'env2 init'
 end
 
 def env2(i2c, display)
@@ -22,19 +30,33 @@ end
 i2c = I2C.new(22, 21)
 display = ILI934X.new(23, 18, 14, 27, 33, 32)
 state = 1
+tof_setup = false
+env2_setup = false
 
 while true
   switches.each_with_index do |sw, i|
-    if sw.read == 1
+    if sw.read == 0
       state = i
     end
   end
   case state
   when 0
-    tof(i2c, display)
+    if tof_setup
+      tof(i2c, display)
+    else
+      tof_init(i2c)
+      tof_setup = true
+    end
   when 1
+    tof_setup = false
+    env2_setup = false
     menu(i2c, display)
   when 2
-    env2(i2c, display)
+    if env2_setup
+      env2(i2c, display)
+    else
+      env2_init(i2c)
+      env2_setup = true
+    end
   end
 end
