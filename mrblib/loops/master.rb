174b94d989e469a68e5c81c1cb9bbeefe30f54b4
rikutons <rikutonsicloud@gmail.com>
2021-09-28 15:41:23 +0900

    Enable env2 display function

--- mrblib/loops/master.rb
+++ mrblib/loops/master.rb
@@ -8,9 +8,9 @@ end
 
 def vl53l0x(i2c, display, vl53l0x)
   range = vl53l0x.read_range_single_millimeters
-  puts range
+  puts "range: #{range}"
   puts " TIMEOUT" if vl53l0x.timeout_occurred
-  display.writeString(range.to_s, 50, 50, 20, 35, 28, ILI934X.color(0x27, 0x1c, 0x19), ILI934X.color(0xff, 0xf3, 0xec))
+  display.writeString(sprintf("%4d", range), 50, 50, 20, 35, 28, ILI934X.color(0x27, 0x1c, 0x19), ILI934X.color(0xff, 0xf3, 0xec))
 end
 
 def menu(i2c, display)
@@ -21,17 +21,32 @@ end
 def env2_init(i2c)
   p 'env2 init'
   sht3x = SHT3X.new(i2c)
+  p "sht3x init"
   bmp280 = BMP280.new(i2c)
+  p "bmp280 init"
   return bmp280, sht3x
 end
 
 def env2(i2c, display, bmp280, sht3x)
   temp_s = sht3x.readTemperature
   temp_b = bmp280.readTemperature
-  pressure = bmp280.readTemperature
+  pressure = bmp280.readPressure
   p "#{temp_s} C"
-  p "#{temp_b} C, #{temp_b} Pa" 
+  p "#{temp_b} C, #{pressure} Pa" 
+  draw_back(display)
+  str = sprintf("    %.2f\n    %.2f\n%d", temp_s, temp_b, pressure)
+  display.writeString(str, 75, 75, 14, 31, 28, ILI934X.color(0x27, 0x1c, 0x19), nil)
   sleep 1
+end
+
+def draw_back(display)
+  xs = [70, 130, 190, 250]
+  ys = [70, 120, 170]
+  3.times do |x|
+    2.times do |y|
+      display.drawRectangle(xs[x], xs[x + 1], ys[y], ys[y + 1], ILI934X.color(0xff, 0xf3, 0xec))
+    end
+  end
 end
 
 swpins = [39, 38, 37]
@@ -43,52 +58,31 @@ end
 i2c = I2C.new(22, 21)
 vl53l0x = nil
 display = ILI934X.new(23, 18, 14, 27, 33, 32)
-sleep 10
-# (40...300).each do |y|
-#   (30...200).each do |x|
-#     display.drawPixel(x, y, ILI934X.color(0xff, 0xf3, 0xec))
-#   end
-# end
-display.drawRectangle(50, 100, 30, 150, ILI934X.color(0xff, 0xf3, 0xec))
-display.drawRectangle(100, 270, 30, 150, ILI934X.color(0xff, 0xf3, 0xec))
-display.drawRectangle(50, 100, 150, 220, ILI934X.color(0xff, 0xf3, 0xec))
-display.drawRectangle(100, 270, 150, 220, ILI934X.color(0xff, 0xf3, 0xec))
+sleep 5
 state = 1
 bmp280 = nil
 sht3x = nil
-vl53l0x_setup = false
-env2_setup = false
 
 while true
   switches.each_with_index do |sw, i|
-    if sw.read == 0
-      state = i
-    end
+    state = i if sw.read == 0
   end
-
-  # switches.each do |sw|
-  #   p sw.get_pin 
-  # end
-  sleep 0.01
 
   case state
   when 0
-    if vl53l0x_setup
-      vl53l0x(i2c, display, vl53l0x)
-    else
+    if vl53l0x.nil?
       vl53l0x = vl53l0x_init(i2c)
-      vl53l0x_setup = true
+    else
+      vl53l0x(i2c, display, vl53l0x)
     end
   when 1
-    vl53l0x_setup = false
-    env2_setup = false
+    bmp280 = vl53l0x = nil
     menu(i2c, display)
   when 2
-    if env2_setup
-      env2(i2c, display, bmp280, sht3x)
-    else
+    if bmp280.nil?
       bmp280, sht3x = env2_init(i2c)
-      env2_setup = true
+    else
+      env2(i2c, display, bmp280, sht3x)
     end
   end
 end
