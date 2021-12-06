08e4f4b4a5266cfc4aa6d08a3479d59cb28ce666
rikutons <rikutonsicloud@gmail.com>
2021-12-06 18:54:54 +0900

    Comment out slow functions temporarily

--- mrblib/models/ili934x.rb
+++ mrblib/models/ili934x.rb
@@ -88,57 +88,57 @@ class ILI934X
       end
     end
 
-    def writeChar(char, x, y, height = 7, color = self.color(0, 0, 0), background_color = nil)
+    # def writeChar(char, x, y, height = 7, color = self.color(0, 0, 0), background_color = nil)
 
-      # Generated with TTF2BMH
-      # Font MisakiGothic
-      # Font Size: 8
-      font_width = 3
-      font_height = 7
-      width = height / 7 * 3
-      char_addr =
-      {
-        32 => [0, 0, 0], 46 => [0,32,0], 48 => [28,42,28], 49 => [36,62,32], 50 => [50,42,36], 51 => [34,42,20], 52 => [24,20,62], 53 => [46,42,18], 54 => [28,42,18], 55 => [2,58,6], 56 => [20,42,20], 57 => [36,42,28], 58 => [0,36,0]
-      }
-      code = char.ord
-      if code >= 32 && code <= 58 
-        bitmap = char_addr[code]
-      end
+    #   # Generated with TTF2BMH
+    #   # Font MisakiGothic
+    #   # Font Size: 8
+    #   font_width = 3
+    #   font_height = 7
+    #   width = height / 7 * 3
+    #   char_addr =
+    #   {
+    #     32 => [0, 0, 0], 46 => [0,32,0], 48 => [28,42,28], 49 => [36,62,32], 50 => [50,42,36], 51 => [34,42,20], 52 => [24,20,62], 53 => [46,42,18], 54 => [28,42,18], 55 => [2,58,6], 56 => [20,42,20], 57 => [36,42,28], 58 => [0,36,0]
+    #   }
+    #   code = char.ord
+    #   if code >= 32 && code <= 58 
+    #     bitmap = char_addr[code]
+    #   end
 
-      width.times do |dx|
-        height.times do |dy|
-          i = dx * font_width / width
-          j = dy * font_height / height 
-          if bitmap[i] & (1 << j) != 0
-            drawPixel(x + dx, y + dy, color)
-          elsif background_color != nil
-            drawPixel(x + dx, y + dy, background_color)
-          end
-        end
-      end
-    end
+    #   width.times do |dx|
+    #     height.times do |dy|
+    #       i = dx * font_width / width
+    #       j = dy * font_height / height 
+    #       if bitmap[i] & (1 << j) != 0
+    #         drawPixel(x + dx, y + dy, color)
+    #       elsif background_color != nil
+    #         drawPixel(x + dx, y + dy, background_color)
+    #       end
+    #     end
+    #   end
+    # end
 
-    def writeString(str, x, y, margin_x, margin_y, height = 7, color = self.color(0, 0, 0), background_color = nil)
-      home_x = x
-      str.each_char do |c|
-        if c == "\n"
-          x = home_x
-          y += margin_y
-        else
-          writeChar(c, x, y, height, color, background_color)
-          x += margin_x
-        end
-      end
-    end
+    # def writeString(str, x, y, margin_x, margin_y, height = 7, color = self.color(0, 0, 0), background_color = nil)
+    #   home_x = x
+    #   str.each_char do |c|
+    #     if c == "\n"
+    #       x = home_x
+    #       y += margin_y
+    #     else
+    #       writeChar(c, x, y, height, color, background_color)
+    #       x += margin_x
+    #     end
+    #   end
+    # end
 
-    def drawPixel(x, y, color)
-      if x < 0 || x > 320 || y < 0 || y > 240
-        return
-      end
-      set_block(x, x, y, y)
-      @spi.write_command(0x2c)
-      @spi.write_data_word(color)
-    end
+    # def drawPixel(x, y, color)
+    #   if x < 0 || x > 320 || y < 0 || y > 240
+    #     return
+    #   end
+    #   set_block(x, x, y, y)
+    #   @spi.write_command(0x2c)
+    #   @spi.write_data_word(color)
+    # end
 
     def drawRectangle(x1, x2, y1, y2, color)
       set_block(x1, x2, y1, y2)
@@ -165,36 +165,36 @@ class ILI934X
 end
 
 # ILI934X Extend Methods(not in class becouse of memory issue)
-def drawLine(display, x1, x2, y1, y2, color, weight = 1)
-  dx = (x2 - x1).abs
-  dy = (y2 - y1).abs
+# def drawLine(display, x1, x2, y1, y2, color, weight = 1)
+#   dx = (x2 - x1).abs
+#   dy = (y2 - y1).abs
 
-  b = dx < dy 
-  if dx < dy
-    x1, x2, dx, y1, y2, dy = y1, y2, dy, x1, x2, dx
-  end
-  x1, x2, y1, y2 = x2, x1, y2, y1 if x2 < x1
-  sy = (y2 > y1) ? 1 : -1
+#   b = dx < dy 
+#   if dx < dy
+#     x1, x2, dx, y1, y2, dy = y1, y2, dy, x1, x2, dx
+#   end
+#   x1, x2, y1, y2 = x2, x1, y2, y1 if x2 < x1
+#   sy = (y2 > y1) ? 1 : -1
 
-  e = -dx
-  for x in x1..x2 do
-    if !b
-      drawPoint(display, x, y1, color, weight)
-    else
-      drawPoint(display, y1, x, color, weight)
-    end
-    e += 2 * dy
-    if ( e >= 0 )
-      y1 += sy
-      e -= 2 * dx
-    end
-  end
-end
+#   e = -dx
+#   for x in x1..x2 do
+#     if !b
+#       drawPoint(display, x, y1, color, weight)
+#     else
+#       drawPoint(display, y1, x, color, weight)
+#     end
+#     e += 2 * dy
+#     if ( e >= 0 )
+#       y1 += sy
+#       e -= 2 * dx
+#     end
+#   end
+# end
 
-def drawPoint(display, x, y, color, weight = 1)
-  x1 = x - weight / 2
-  x2 = x + weight / 2
-  y1 = y - weight / 2
-  y2 = y + weight / 2
-  display.drawRectangle(x1, x2, y1, y2, color)
-end
+# def drawPoint(display, x, y, color, weight = 1)
+#   x1 = x - weight / 2
+#   x2 = x + weight / 2
+#   y1 = y - weight / 2
+#   y2 = y + weight / 2
+#   display.drawRectangle(x1, x2, y1, y2, color)
+# end
