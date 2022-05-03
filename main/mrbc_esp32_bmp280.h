235bb41e6944c30f526790933c85842fa3972567
sugiymki <sugiyama@gfd-dennou.org>
2022-05-03 20:41:07 +0900

    because of creating new gem

--- main/mrbc_esp32_bmp280.h
+++
@@ -1,8 +0,0 @@
-#ifndef MRBC_ESP32_BMP280_H
-#define MRBC_ESP32_BMP280_H
-
-#include "mrubyc.h"
-
-void mrbc_mruby_esp32_bmp280_gem_init(struct VM*);
-
-#endif // MRBC_ESP32_BMP280_H
--- main/mrbc_esp32_vl53l0x.h
+++
@@ -1,8 +0,0 @@
-#ifndef MRBC_ESP32_VL53L0X_H
-#define MRBC_ESP32_VL53L0X_H
-
-#include "mrubyc.h"
-
-void mrbc_mruby_esp32_vl53l0x_gem_init(struct VM*);
-
-#endif // MRBC_ESP32_VL53L0X_H
