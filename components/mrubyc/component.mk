83493a3e5acdb45a9ed04a4c8ced50f1c9e66b9f
sugiymki <sugiyama@gfd-dennou.org>
2022-05-07 18:39:18 +0900

    change directry tree

--- components/mrubyc/component.mk
+++ components/mrubyc/component.mk
@@ -1,3 +1,3 @@
 CFLAGS = -mlongcalls -DMRBC_DEBUG
-COMPONENT_ADD_INCLUDEDIRS := mrubyc_src
-COMPONENT_SRCDIRS := mrubyc_src mrubyc_src/hal
+COMPONENT_ADD_INCLUDEDIRS := src
+COMPONENT_SRCDIRS := src src/hal
