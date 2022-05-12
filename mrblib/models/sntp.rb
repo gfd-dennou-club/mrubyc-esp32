feeb6b75121aa5fb70cc04c69730b00e33351247
sugiymki <sugiyama@gfd-dennou.org>
2022-05-12 23:11:19 +0900

    update SNTP class

--- mrblib/models/sntp.rb
+++ mrblib/models/sntp.rb
@@ -1,11 +1,6 @@
 # coding: utf-8
 
 class SNTP
-
-  # 内部関数
-  def to_bcd( tt )
-    (tt / 10).to_i(2) << 4 | (tt % 10).to_i(2)
-  end
     
   # 初期化
   def initialize()
@@ -44,38 +39,5 @@ class SNTP
   def sec()
     @time[6]
   end
-  
-  # 時刻取得 (BCD コード)
-  def year_bcd()
-    to_bcd( @time[0] + 1900 )
-  end
 
-  def year2_bcd()
-    to_bcd( (@time[0] + 1900) % 2000 )
-  end
-
-  def mon_bcd()
-    to_bcd( @time[1] )
-  end
-
-  def mday_bcd()
-    to_bcd( @time[2] )
-  end
-
-  def wday_bcd()
-    to_bcd( @time[3] )
-  end
-
-  def hour_bcd()
-    to_bcd( @time[4] )
-  end
-
-  def min_bcd()
-    to_bcd( @time[5] )
-  end
-
-  def sec_bcd()
-    to_bcd( @time[6] )
-  end
-  
 end
