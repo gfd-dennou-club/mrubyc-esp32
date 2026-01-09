c50c4dda3866f090f83727b672bdca8d690b2761
Ko-ichiro Sugiyama <sugiyama@gfd-dennou.org>
2026-01-09 11:54:14 +0900

    modified using Suzuki-san's comments

--- components/mrubyc-3.4.0/hal/esp32/hal.c
+++ components/mrubyc-3.4.0/hal/esp32/hal.c
@@ -1,116 +1,149 @@
 /*! @file
   @brief
   Hardware abstraction layer
-        for ESP32 (mruby/c)
-        Updated for ESP-IDF v5.x GPTimer
+        for ESP32
+
+  <pre>
+  Copyright (C) 2015- Kyushu Institute of Technology.
+  Copyright (C) 2015- Shimane IT Open-Innovation Center.
+
+  This file is distributed under BSD 3-Clause License.
+  </pre>
 */
 
+/***** Feature test switches ************************************************/
 /***** System headers *******************************************************/
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include "esp_types.h"
-#include "esp_attr.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "soc/timer_group_struct.h"
 #include "driver/gptimer.h"
+
 
 /***** Local headers ********************************************************/
 #include "hal.h"
 
+
+/***** Constat values *******************************************************/
+#ifndef MRBC_NO_TIMER
+#define GPTIMER_RESOLUTION (1000*1000) // 1 MHz
+#endif
+
+/***** Macros ***************************************************************/
+/***** Typedefs *************************************************************/
+/***** Function prototypes **************************************************/
 /***** Local variables ******************************************************/
 static portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
-static gptimer_handle_t gptimer = NULL;
 
+
+/***** Global variables *****************************************************/
+/***** Signal catching functions ********************************************/
 /***** Local functions ******************************************************/
 #ifndef MRBC_NO_TIMER
-/**
- * @brief タイマーアラーム発生時のコールバック関数
- * ISR (Interrupt Service Routine) コンテキストで実行されます
- */
-static bool IRAM_ATTR on_timer_alarm_cb(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_ctx)
+//================================================================
+/*!@brief
+  Timer ISR function
+
+*/
+static bool on_timer(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_ctx)
 {
-    // mruby/c のチック処理を呼び出し
     mrbc_tick();
-    
-    // 他のタスクをウェイクアップさせる必要がない場合は false を返す
     return false;
 }
+//================================================================
+/*!@brief
+  Converting ms value to timer ticks.
+
+*/
+#define GPTIMER_FROM_MS(ms) ((ms)*GPTIMER_RESOLUTION/1000)
 #endif
+
 
 /***** Global functions *****************************************************/
 #ifndef MRBC_NO_TIMER
 
-/**
- * @brief 初期化
- */
+//================================================================
+/*!@brief
+  initialize
+
+*/
 void hal_mrubyc_init(void)
 {
-    // 1. タイマー設定 (1MHz = 1 tick 1us)
-    gptimer_config_t timer_config = {
-        .clk_src = GPTIMER_CLK_SRC_DEFAULT,
-        .direction = GPTIMER_COUNT_UP,
-        .resolution_hz = 1000000, 
-    };
-    ESP_ERROR_CHECK(gptimer_new_timer(&timer_config, &gptimer));
-
-    // 2. アラーム設定 (MRBC_TICK_UNIT * 1000 us)
-    gptimer_alarm_config_t alarm_config = {
-        .alarm_count = MRBC_TICK_UNIT * 1000, 
-        .reload_count = 0,
-        .flags.auto_reload_on_alarm = true,
-    };
-    ESP_ERROR_CHECK(gptimer_set_alarm_action(gptimer, &alarm_config));
-
-    // 3. コールバック登録 (ここを修正しました)
-    gptimer_event_callbacks_t cbs = {
-        .on_alarm = on_timer_alarm_cb,
-    };
-    // gptimer_register_callbacks ではなく gptimer_register_event_callbacks が正解です
-    ESP_ERROR_CHECK(gptimer_register_event_callbacks(gptimer, &cbs, NULL));
-
-    // 4. タイマー有効化と開始
-    ESP_ERROR_CHECK(gptimer_enable(gptimer));
-    ESP_ERROR_CHECK(gptimer_start(gptimer));
+  gptimer_handle_t gptimer = NULL;
+  gptimer_config_t timer_config = {
+    .clk_src = GPTIMER_CLK_SRC_DEFAULT,
+    .direction = GPTIMER_COUNT_UP,
+    .resolution_hz = GPTIMER_RESOLUTION,
+  };
+  ESP_ERROR_CHECK(gptimer_new_timer(&timer_config, &gptimer));
+  gptimer_alarm_config_t alarm_config = {
+    .reload_count = 0,
+    .alarm_count = GPTIMER_FROM_MS(MRBC_TICK_UNIT),
+    .flags.auto_reload_on_alarm = true
+  };
+  ESP_ERROR_CHECK(gptimer_set_alarm_action(gptimer, &alarm_config));
+  gptimer_event_callbacks_t cbs = {
+    .on_alarm = on_timer
+  };
+  ESP_ERROR_CHECK(gptimer_register_event_callbacks(gptimer, &cbs, NULL));
+  ESP_ERROR_CHECK(gptimer_enable(gptimer));
+  ESP_ERROR_CHECK(gptimer_start(gptimer));
 }
 
-/**
- * @brief 割り込み許可
- */
+
+//================================================================
+/*!@brief
+  enable interrupt
+
+*/
 void hal_enable_irq(void)
 {
-    portEXIT_CRITICAL(&mux);
+  portEXIT_CRITICAL(&mux);
 }
 
-/**
- * @brief 割り込み禁止
- */
+
+//================================================================
+/*!@brief
+  disable interrupt
+
+*/
 void hal_disable_irq(void)
 {
-    portENTER_CRITICAL(&mux);
+  portENTER_CRITICAL(&mux);
 }
+
 
 #endif /* ifndef MRBC_NO_TIMER */
 
+//================================================================
+/*!@brief
+  write to stdio (UART0 or UART2)
+
+*/
 int hal_write(int fd, const void *buf, int nbytes)
 {
-  // stdout ポインタが指す先（UART0 or UART2）に対して、nbytes 分まとめて書き込む
   size_t ret = fwrite(buf, 1, nbytes, stdout);
-  
-  // バッファリングを無効化していない場合に備えて、即時出力させる
-  fflush(stdout); 
-  
+  fflush(stdout);   
   return (int)ret;
 }
 
-/**
- * @brief プログラム異常終了
- */
+
+//================================================================
+/*!@brief
+  abort program
+
+  @param s	additional message.
+*/
 void hal_abort(const char *s)
 {
   if( s ) {
     //write(1, s, strlen(s));
     hal_write(1, s, strlen(s));
   }
+
   abort();
 }
 
